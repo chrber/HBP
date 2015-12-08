764cbdc62b9f5aee5e9f65a414164b8ec5b33421
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2015-12-08 21:58:53 +0100

    Add comments in master script.

--- master.sh
+++ master.sh
@@ -4,8 +4,9 @@ WORKER_NUM=${1}
 WORKER_PIDS=""
 
 for WORKER_ID in `seq 1 ${WORKER_NUM}`; do
-   #./worker.sh ${WORKER_ID} ${2} &
-   ./worker.py &
+   #./worker.sh ${WORKER_ID} ${2} & # bash worker
+   ./worker.py & # first python worker (thread pool)
+   #./worker2.py & # second python worker (separate threads)
    WORKER_PIDS="${WORKER_PIDS} $!"
 done
 
