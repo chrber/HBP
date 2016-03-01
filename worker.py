d799f3a7bd0b88a180d67121815e0d406d0ad55e
Christian Bernardt <christian.bernardt@desy.de>
2016-03-01 12:10:01 +0100

    Add single request times output

--- worker.py
+++ worker.py
@@ -8,7 +8,8 @@ import time
 
 THREAD_NUM = 8
 # ENDPOINT = '149.156.9.143:8888/image/v0/api/bbic?fname=/srv/data'
-ENDPOINT = 'dcache-dot12.desy.de:8888/image/v0/api/bbic?fname=/srv/data/HBP'
+#ENDPOINT = 'dcache-dot12.desy.de:8888/image/v0/api/bbic?fname=/srv/data/HBP'
+ENDPOINT = 'hbp-image.desy.de:8888/image/v0/api/bbic?fname=/srv/data/HBP/'
 
 def do_request((stack, level, slice, x, y)):
 	url = 'http://{endpoint}/BigBrain_jpeg.h5&mode=ims&prog=' \
@@ -20,7 +21,10 @@ def do_request((stack, level, slice, x, y)):
 		x = x,
 		y = y
 	)
+        start = time.time()
 	r = requests.get(url)
+        end = time.time()
+        diff = int((end - start) * 1000)
 	# print('stack = {stack}, level = {level}, slice = {slice}, x = {x}, y = {y}, '\
 	# 	'code = {code}'.format(
 	# 		stack = stack,
@@ -31,7 +35,7 @@ def do_request((stack, level, slice, x, y)):
 	# 		code = r.status_code
 	# 		)
 	# 	)
-	return r.status_code
+	return r.status_code, diff, url
 
 def random_params():
 	level = random.randint(0, 2)
@@ -49,8 +53,11 @@ def main():
 	results = pool.map(do_request, params)
 	end = time.time()
 	diff = int((end - start) * 1000)
-	errors = len(results) - results.count(200)
+	#errors = len(results) - results.count(200)
+	errors = len(results) - len(filter(lambda x: x[0] == 200, results))
 	print('time: {0} ms, errors: {1} / {2}'.format(diff, errors, len(results)))
+        for r in results:
+           print("diff: %d %s" % (r[1], r[2]))
 
 if __name__ == '__main__':
 	main()
