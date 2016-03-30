8d42ef1858b01671588470876a9a7ef30a97b95f
Christian Bernardt <christian.bernardt@desy.de>
2016-03-30 18:13:07 +0200

    Enable cyfronet endpoint

--- worker.py
+++ worker.py
@@ -7,9 +7,9 @@ import requests
 import time
 
 THREAD_NUM = 8
-#ENDPOINT = '149.156.9.143:8888/image/v0/api/bbic?fname=/srv/data'
+ENDPOINT = '149.156.9.143:8888/image/v0/api/bbic?fname=/srv/data'
 #ENDPOINT = 'dcache-dot12.desy.de:8888/image/v0/api/bbic?fname=/srv/data/HBP'
-ENDPOINT = 'hbp-image.desy.de:8888/image/v0/api/bbic?fname=/srv/data/HBP/'
+#ENDPOINT = 'hbp-image.desy.de:8888/image/v0/api/bbic?fname=/srv/data/HBP/'
 
 def do_request((stack, level, slice, x, y)):
 	url = 'http://{endpoint}/BigBrain_jpeg.h5&mode=ims&prog=' \
